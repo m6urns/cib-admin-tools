6822c7e8c9098d444cb07939be8b00981b6d7072
Matt Burns <1515005+m6urns@users.noreply.github.com>
2024-04-08 13:19:32 -0600

    Fix cert path

--- renew-bioinfo-cert.sh
+++ renew-bioinfo-cert.sh
@@ -4,7 +4,7 @@
 certbot renew --force-renewal
 
 # Concat the certs together, and place them in the haproxy directory
-bash -c "cat /etc/letsencrypt/live/bioinfocore.usu.edu/fullchain.pem /etc/letsencrypt/live/bioinfocore.usu.edu/privkey.pem > /etc/ssl/certs/haproxy/haproxy.le.pem"
+bash -c "cat /etc/letsencrypt/live/bioinfocore.usu.edu/fullchain.pem /etc/letsencrypt/live/bioinfocore.usu.edu/privkey.pem > /etc/ssl/certs/haproxy/haproxy.le/haproxy.le.pem"
 
 # Restart haproxy
 systemctl restart haproxy
